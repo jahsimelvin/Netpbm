ae6c9827e68b0e5d67de87592d5a6186db83f31c
Your Namemelvin <melvin.jahsi@ynov.com>
2024-01-09 14:32:38 +0100

    v1

--- test/test.go
+++ test/test.go
@@ -7,7 +7,8 @@ import (
 )
 
 func main() {
-	pbm, err := netpbm.ReadPBM("test.pbm")
+	filename := "image1.pbm"
+	pbm, err := netpbm.ReadPBM(filename)
 	if err != nil {
 		fmt.Println("error", err)
 		return
@@ -16,8 +17,9 @@ func main() {
 	fmt.Printf("Magic Number: %s\n", pbm.MagicNumber)
 	fmt.Printf("Width: %d\n", pbm.Width)
 	fmt.Printf("Height: %d\n", pbm.Height)
-	fmt.Printf("Data:")
+	fmt.Println("Data:")
 	for _, row := range pbm.Data {
 		fmt.Println(row)
 	}
+
 }
